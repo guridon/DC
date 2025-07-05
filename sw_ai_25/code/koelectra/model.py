c86ec9c1efe9766692c60d0f86129de0ca0833e8
guridon <sujin3824@naver.com>
2025-07-05 12:57:50 +0900

    Remove unused code and clean up repo

--- sw_ai_25/code/koelectra/model.py
+++
@@ -1,9 +0,0 @@
-# model.py
-from transformers import AutoModelForSequenceClassification
-from config import Config
-
-def get_model():
-    return AutoModelForSequenceClassification.from_pretrained(
-        Config.model_name,
-        num_labels=Config.num_labels
-    )
